180aad5c556ebc87b30effa34f3e40bbf49a9d96
berkterek <42406263+berkterek@users.noreply.github.com>
2022-03-19 11:29:10 +0300

    refactor: character can move horizontal with old input system

--- Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -5,6 +5,24 @@ namespace Platformer2d.Controllers
 {
     public class PlayerController : MonoBehaviour, IPlayerController
     {
+        [SerializeField] float _moveSpeed = 1f;
         
+        Transform _transform;
+        float _horizontal;
+
+        void Awake()
+        {
+            _transform = transform;
+        }
+
+        void Update()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+        }
+
+        void FixedUpdate()
+        {
+            _transform.Translate(Vector3.right * _horizontal * Time.deltaTime * _moveSpeed);
+        }
     }
 }
