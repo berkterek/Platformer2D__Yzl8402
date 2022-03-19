eb2d67fa7b0f00ee81db1ea72fec5d2947eb5eae
berkterek <42406263+berkterek@users.noreply.github.com>
2022-03-19 11:23:44 +0300

    feat: first script and player prefab created

---
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Abstracts/Controllers/IEntityController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2d.Abstracts.Controllers
+{
+    public interface IEntityController
+    {
+        Transform transform { get; }
+    }
+}
+
---
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,10 @@
+using Platformer2d.Abstracts.Controllers;
+using UnityEngine;
+
+namespace Platformer2d.Controllers
+{
+    public class PlayerController : MonoBehaviour, IPlayerController
+    {
+        
+    }
+}
