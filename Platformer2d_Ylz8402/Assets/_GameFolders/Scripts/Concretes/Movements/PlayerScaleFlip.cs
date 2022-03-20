b219c692958fdfaee2e488e3ebe48cdaee49fe1a
berkterek <42406263+berkterek@users.noreply.github.com>
2022-03-20 10:42:12 +0300

    feat: player can flip with body local scale

---
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Movements/PlayerScaleFlip.cs
@@ -0,0 +1,36 @@
+﻿using Platformer2d.Abstracts.Controllers;
+using UnityEngine;
+
+namespace Platformer2d.Movements
+{
+    public class PlayerScaleFlip : IFlip
+    {
+        readonly IPlayerController _playerController;
+        readonly Transform _transform;
+
+        float _horizontal;
+        
+        public PlayerScaleFlip(IPlayerController playerController)
+        {
+            Debug.Log($"Flip => {nameof(PlayerScaleFlip)}");
+            _playerController = playerController;
+            _transform = _playerController.transform.GetChild(0).transform;
+        }
+        
+        public void Tick()
+        {
+            _horizontal = _playerController.InputReader.Horizontal;
+        }
+
+        public void LateTick()
+        {
+            if (_horizontal == 0f) return;
+
+            _horizontal = _horizontal > 0f ? 1f : -1f;
+
+            if (_horizontal == _transform.localScale.x) return;
+
+            _transform.localScale = new Vector3(_horizontal, 1f, 1f);
+        }
+    }
+}
