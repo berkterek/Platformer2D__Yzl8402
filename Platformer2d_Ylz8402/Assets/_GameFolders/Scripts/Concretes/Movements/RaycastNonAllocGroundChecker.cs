8fc8060012d882cd0135be3e310449c569c7be59
berkterek <42406263+berkterek@users.noreply.github.com>
2022-03-20 11:18:31 +0300

    refactor: physics layers reordered

---
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Movements/RaycastNonAllocGroundChecker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Platformer2d.Movements
+{
+    public class RaycastNonAllocGroundChecker : IGroundChecker
+    {
+        Transform[] _transforms;
+        RaycastHit2D[] _hitResults;
+        
+        public bool IsOnGround { get; private set; }
+
+        public RaycastNonAllocGroundChecker()
+        {
+            _hitResults = new RaycastHit2D[10];
+        }
+        
+        public void FixedTick()
+        {
+            foreach (Transform transform in _transforms)
+            {
+                int resultCount = Physics2D.RaycastNonAlloc(
+                    transform.position,
+                    transform.forward,
+                    _hitResults,
+                    0.1f,
+                );    
+            }
+        }
+    }
+
+    public interface IGroundChecker
+    {
+        bool IsOnGround { get; }
+        void FixedTick();
+    }
+}
