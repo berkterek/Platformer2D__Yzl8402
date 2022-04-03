36d716adcf8f42d20a6557d1e66ffd81838b67d4
berkterek <42406263+berkterek@users.noreply.github.com>
2022-04-03 13:54:49 +0300

    feat: key chest created and animation set

---
+++ Platformer2d_Ylz8402/Assets/_GameFolders/Scripts/Concretes/Controllers/KeyChestController.cs
@@ -0,0 +1,25 @@
+﻿using Platformer2d.Abstracts.Controllers;
+using UnityEngine;
+
+namespace Platformer2d.Controllers
+{
+    public class KeyChestController : MonoBehaviour
+    {
+        Animator _animator;
+        Collider2D _collider2D;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _collider2D = GetComponent<Collider2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.TryGetComponent(out IPlayerController playerController)) return;
+            
+            _animator.SetTrigger("open");
+            _collider2D.enabled = false;
+        }
+    }
+}
